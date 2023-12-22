1341f7a6ab24ada800d8c42d0923e457fc877072
matheusaprigio11 <aprigiomatheus24@gmail.com>
2023-12-22 11:13:08 -0300

    updt

---
+++ 2_setup/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Bomdia from './components/Bomdia'
+import Welcome from './components/Welcome'
+function App() {
+  
+  return (
+    <>
+      <Welcome />
+      <Bomdia />
+    </>
+  )
+}
+
+export default App
---
+++ 2_setup/src/components/Bomdia.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Bomdia = () => {
+    const NAME = "Matheus"
+  return (
+    <div>Bomdia, {NAME}, tudo certo?</div>
+  )
+}
+
+export default Bomdia
