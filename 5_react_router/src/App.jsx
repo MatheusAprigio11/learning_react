fb0237cff1357066da4c92f70ba341ac2eac8f46
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-01-24 13:18:58 -0300

    add Router DOM

---
+++ 5_react_router/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import { Routes, Route, Link, NavLink } from "react-router-dom"
+
+function App() {
+
+
+  return (
+    <>
+     <Routes>
+        <Route path="/" element={} />
+     </Routes>
+    </>
+  )
+}
+
+export default App
